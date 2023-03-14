1f333d3c83fd2fc65d1e1e4404e249e5157b39cb
Ugo <alexarinzeugo@yahoo.com>
2023-03-14 23:16:42 +0000

    add file 100 and other test files

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import calculator_1
+    import sys
+
+    add = calculator_1.add
+    sub = calculator_1.sub
+    mul = calculator_1.mul
+    div = calculator_1.div
+
+    argCount = len(sys.argv) - 1
+
+    if argCount != 3:
+        print("{}".format("Usage: ./100-calculator.py <a> <operator> <b>"))
+        exit(1)
+
+    operator = sys.argv[2]
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+
+    if operator == '+':
+        print("{:d} + {:d} = {:d}".format(a, b, add(a,b)))
+    elif operator == '-':
+        print("{:d} - {:d} = {:d}".format(a, b, sub(a,b)))
+    elif operator == '*':
+        print("{:d} * {:d} = {:d}".format(a, b, mul(a,b)))
+    elif operator == '/':
+        print("{:d} / {:d} = {:d}".format(a, b, div(a,b)))
+    else:
+        print("{:s}".format("Unknown operator. Available operators: +, -, * and /"))
+        exit(1)
