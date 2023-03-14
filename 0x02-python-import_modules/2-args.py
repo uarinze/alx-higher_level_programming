4140c5f217da792b501003fa183ee9a54a86c63d
Ugo <alexarinzeugo@yahoo.com>
2023-03-14 21:52:25 +0000

    add file 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import sys
+
+    argCount = len(sys.argv) - 1
+    if (argCount) == 0:
+        print("{:d} {:s}.".format(argCount, "arguments"))
+    elif (argCount) == 1:
+        print("{:d} {:s}:".format(argCount, "argument"))
+    else:
+        print("{:d} {:s}:".format(argCount, "arguments"))
+    for i in range(argCount):
+        print("{:d} : {}".format(i + 1, sys.argv[i + 1]))
