a70c3dfd52e5b352887e1867d6e4b09148a06bfa
Ugo <alexarinzeugo@yahoo.com>
2023-03-15 02:29:00 +0000

    edit 10

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -5,7 +5,7 @@ def divisible_by_2(my_list=[]):
     for i in range(len(my_list)):
         if my_list[i] % 2 == 0:
             multiples.append(True)
-         else:
+        else:
              multiples.append(False)
     
     return (multiples)
