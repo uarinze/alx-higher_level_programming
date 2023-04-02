2332471f437b999cea6e70a5ad846609ba00dba7
Ugo <alexarinzeugo@yahoo.com>
2023-04-02 05:14:09 +0000

    added file 6

---
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+def raise_exception_msg(message=""):
+    '''
+    This function raises a name exception with a message
+    Args:
+        message: the message to pass
+    Returns:
+        None
+    '''
+    raise NameError(message)
