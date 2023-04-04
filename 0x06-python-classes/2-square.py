209a4983a4059a5738f62ac942f5848d2f5684fc
Ugo <alexarinzeugo@yahoo.com>
2023-04-04 22:47:23 +0000

    added file 2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""This is a class that defines a square"""
+
+
+class Square:
+    """This is a sqaure"""
+    def __init__(self, size = 0):
+        """The init method that initialises the fields for each instance.
+
+        Args:
+            size: this is an optional int field.
+        """
+        if (isinstance(size, int) != True):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
