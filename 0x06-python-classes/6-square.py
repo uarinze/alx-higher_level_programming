c9fb3ca8b18b7ff38d2edc365c25d326af18cc43
Ugo <alexarinzeugo@yahoo.com>
2023-04-05 02:02:52 +0000

    edited file 6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -36,9 +36,9 @@ class Square:
     @position.setter
     def position(self, value):
         """this sets the position"""
-        if not isinstance(value, tuple)
-                or any(idx < 0 for idx in value)
-                or len(value) is not 2
+        if not isinstance(value, tuple) \
+                or any(idx < 0 for idx in value) \
+                or len(value) is not 2 \
                 or not all(isinstance(ind, int) for ind in value):
             raise TypeError("position must be a tupple\
                     of two positive integers")
