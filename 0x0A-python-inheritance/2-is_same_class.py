7d18f03760ac3d827ab59ff95fe40f9e69671291
Ugo <alexarinzeugo@yahoo.com>
2023-04-13 07:04:29 +0000

    added doc string to file2

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python
 
+    """Function that returns true if the
+    object is exactly an instance of the specified class;
+    otherwise false.
+    """
 
 def is_same_class(obj, a_class):
     """Function that returns true if the
