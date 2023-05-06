1699d666952e6d40d6ec6ae5567f3571bd6a7efc
Ugo <alexarinzeugo@yahoo.com>
2023-05-06 21:28:08 +0000

    remove whiteline

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -7,7 +7,7 @@
 def append_write(filename="", text=""):
     '''function that appends a string at the end of a text file
     (UTF8) and returns the number of characters added
-    
+
     Args:
         filename: name of file
         text: the text to apend
