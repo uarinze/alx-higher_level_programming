43c2c9b612438b92b4d3161cb777deffb9cea830
Ugo <alexarinzeugo@yahoo.com>
2023-05-07 04:40:02 +0000

    added fies 4

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+'''function that returns an object (Python data structure)
+represented by a JSON string
+'''
+import json
+
+
+def from_json_string(my_str):
+    '''function that returns an object (Python data structure)
+    represented by a JSON string
+    '''
+    jobj = json.loads(my_str)
+    return jobj
