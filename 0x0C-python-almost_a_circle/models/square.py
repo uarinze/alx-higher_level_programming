5fb2784208ce0b48233546b3e9f7f81d99793687
Ugo <alexarinzeugo@yahoo.com>
2023-05-14 12:42:43 +0000

    added square class

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+'''square module'''
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    '''The class square inherits from the rectangle class/'''
+
+    def __init__(self, size, x=0, y=0, id=None):
+        '''initialisation with superclass attributes'''
+        super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        '''getter for size'''
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        '''settet for size'''
+        self.width = value
+        self.height = value
+
+    def __str__(self):
+        '''string representation fo the instance'''
+        return "[{}] ({}) {}/{} - {}".format(self.__class__.__name__, self.id, self.x, self.y, self.size)
