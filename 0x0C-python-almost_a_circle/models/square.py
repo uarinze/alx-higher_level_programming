4d091076a0b62a51b56b223af51fadaec8bebff0
Ugo <alexarinzeugo@yahoo.com>
2023-05-14 12:45:53 +0000

    editted a long line for pycodestyle

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -23,4 +23,5 @@ class Square(Rectangle):
 
     def __str__(self):
         '''string representation fo the instance'''
-        return "[{}] ({}) {}/{} - {}".format(self.__class__.__name__, self.id, self.x, self.y, self.size)
+        return "[{}] ({}) {}/{} - {}".format(
+                self.__class__.__name__, self.id, self.x, self.y, self.size)
