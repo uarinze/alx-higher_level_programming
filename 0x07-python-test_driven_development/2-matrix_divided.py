50fd95878fc273b8f26613fd29c73e4443e4b03a
Ugo <alexarinzeugo@yahoo.com>
2023-09-09 02:28:04 +0000

    eddited file2

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -3,11 +3,23 @@
 
 
 def matrix_divided(matrix, div):
-    """Function divides the elements of a matrix"""
-    if type(matrix) is not list or matrix == [] \
+    """Function divides the elements of a matrix
+    Args:
+        matrix (list): A list of lists of ints or floats.
+        div (int/float): The divisor
+    Raises:
+        TypeError: If the matrix contains non-numbers.
+        TypeError: If the matrix contains rows of different sizes.
+        TypeError: If div is not an int or float.
+        ZeroDivisionError: If div is 0.
+    Returns:
+    A new matrix representing the result of the division.
+    """
+    #if type(matrix) is not list or matrix == [] \
+    if (not isinstance(matrix, list) or matrix == []\       
             or not all(isinstance(row, list) for row in matrix) \
             or not all((isinstance(num, int) or isinstance(num, float))
-                    for num in [item for row in matrix for item in row]):
+                     for num in [item for row in matrix for item in row]):
         raise TypeError("matrix must be a matrix \
         (list of list) of integers/floats")
     if type(div) not in [int, float]:
