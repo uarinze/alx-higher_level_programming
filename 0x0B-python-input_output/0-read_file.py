92c0fff3bea3c8b2eec44918a3053fda0af5dd57
Ugo <alexarinzeugo@yahoo.com>
2023-09-12 20:22:43 +0000

    added file 0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+'''Function reads a text file and prints to stdout'''
+
+
+def read_file(filename=""):
+    ''' This function reads a file's content to stdout'''
+    with open(filename, 'r', encoding="UTF-8") as f:
+        print(f.read(), end="")
