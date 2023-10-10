67a9d73fd276a187c1af6044935bceddf6667f9d
Ugo <alexarinzeugo@yahoo.com>
2023-10-10 18:31:45 +0000

    added kwargs

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -25,5 +25,35 @@ class Square(Rectangle):
         '''string representation fo the instance'''
         return "[{}] ({}) {}/{} - {}".format(
                 self.__class__.__name__, self.id, self.x, self.y, self.size)
-#    def update(self, *args, **kwargs):
-#        '''this assigns attributes to the square class''
+
+    def update(self, *args, **kwargs):
+        '''this assigns attributes to the square class'''
+        if args and  len(args) != 0:
+            count = 0
+            for i in args:
+                if count == 0:
+                    if args is None:
+                        self.__init__(self.size, self.x, self.y)
+                    else:
+                        self.id = i
+                elif count == 1:
+                    self.size = i
+                elif count == 2:
+                    self.x = i
+                elif count == 3:
+                    self.y = i
+                count += 1
+
+        elif kwargs and len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k == "id":
+                    if v is None:
+                        self.__init__(self.size, self.x, self.y)
+                    else:
+                        self.id = v
+                elif k == "size":
+                    self.size = v
+                elif k == "x":
+                    self.x = v
+                elif k == "y":
+                    self.y = v
