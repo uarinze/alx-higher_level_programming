f21731157e30921a6cbec5d7d8ea9b0320be1b84
Ugo <alexarinzeugo@yahoo.com>
2023-10-11 14:11:35 +0000

    update base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -33,3 +33,11 @@ class Base:
             else:
                 list_dicts = [o.to_dictionary() for o in list_objs]
                 jsonfile.write(Base.to_json_string(list_dicts))
+
+    @staticmethod
+    def from_json_string(json_string):
+        '''returns the list of JSON string representing json_string.'''
+        if json_string is None or json_string == "[]":
+            return []
+        else:
+            return json.loads(json_string)
