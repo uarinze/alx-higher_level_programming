e524f8509d89d4543d3fac0b867322fc68c9fef3
Ugo <alexarinzeugo@yahoo.com>
2024-07-02 17:10:34 +0000

    added files

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const args = process.argv;
+const x = Math.floor(Number(args[2]));
+const y = Math.floor(Number(args[3]));
+function add (a, b) {
+  const sum = a + b;
+  return sum;
+}
+console.log(add(x, y));
